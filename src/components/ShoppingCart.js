ad6833a4a592a494da1af2f68f54dffe4b7b7cdc
Jonathan Cheng <jcheng461@gmail.com>
2022-12-13 23:24:05 +0900

    WIP: add ShoppingCart.js component for UI

---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ShoppingCart() {
+  return (
+    <React.Fragment>
+      <h2>Shopping Cart</h2>
+    </React.Fragment>
+  );
+}
+
+export default ShoppingCart;
