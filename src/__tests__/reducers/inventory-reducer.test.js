0b3718defcf487038a9b3a9cd20ff414f5baebdf
Jonathan Cheng <jcheng461@gmail.com>
2022-12-28 10:19:19 +0900

    reducer successfully adds new item data to mainItemList

--- src/__tests__/reducers/inventory-reducer.test.js
+++ src/__tests__/reducers/inventory-reducer.test.js
@@ -1,7 +1,34 @@
 import inventoryReducer from "../../reducers/inventory-reducer";
 
 describe("inventoryReducer", () => {
+  let action;
+  const itemData = {
+    name: "Great Ball",
+    description: "A ball that has a greater capture rate than a regular Poke Ball.",
+    quantity: 1,
+    id: 1,
+  };
+
   test("Should return default state if no action type passed into reducer", () => {
     expect(inventoryReducer({}, { type: null })).toEqual({});
+  });
+
+  test("Should successfully add new item data to mainItemList", () => {
+    const { name, description, quantity, id } = itemData;
+    action = {
+      type: "ADD_ITEM",
+      name: name,
+      description: description,
+      quantity: quantity,
+      id: id,
+    };
+    expect(inventoryReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        description: description,
+        quantity: quantity,
+        id: id,
+      },
+    });
   });
 });
